2d2eb3402f98cb710f44ffbf69006fac11abd044
djawny <djawny@op.pl>
2017-01-23 20:50:53 +0100

    Initial commit

---
+++ movierentalcore/src/main/java/pl/sda/Movie.java
@@ -0,0 +1,21 @@
+package pl.sda;
+
+/**
+ * Created by RENT on 2017-01-23.
+ */
+public class Movie {
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Movie(String title) {
+
+        this.title = title;
+    }
+}
---
+++ movierentalgui/src/main/java/pl/sda/RentalWindow.java
@@ -0,0 +1,7 @@
+package pl.sda;
+
+public class RentalWindow {
+    MovieRental movieRental;
+    MovieRentla2 movieRentla2;
+    MovieRentla3 movieRentla3;
+}
