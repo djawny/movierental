80d9e6ac286d9f1422c77684a7ec2df203a59a32
djawny <djawny@op.pl>
2017-01-24 19:21:34 +0100

    24/01

--- movierentalcore/src/main/java/zad2/Holiday.java
+++ movierentalcore/src/main/java/zad2/Holiday.java
@@ -1,15 +1,17 @@
 package zad2;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 
 public class Holiday {
     private String name;
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private Date date;
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private Date observed;
-    private Boolean public;
+    @JsonProperty("public")
+    private Boolean isPublic;
 
 }
--- movierentalcore/src/main/java/zad2/HolidaysCalendar.java
+++ movierentalcore/src/main/java/zad2/HolidaysCalendar.java
@@ -3,12 +3,13 @@ package zad2;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public class HolidaysCalendar {
     private int status;
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
-    private Map<Date,Holiday> holidays;
+    private Map<Date,List<Holiday>> holidays;
 
     public int getStatus() {
         return status;
@@ -18,11 +19,19 @@ public class HolidaysCalendar {
         this.status = status;
     }
 
-    public Map<Date, Holiday> getHolidays() {
+    public Map<Date, List<Holiday>> getHolidays() {
         return holidays;
     }
 
-    public void setHolidays(Map<Date, Holiday> holidays) {
+    public void setHolidays(Map<Date, List<Holiday>> holidays) {
         this.holidays = holidays;
+    }
+
+    @Override
+    public String toString() {
+        return "HolidaysCalendar{" +
+                "status=" + status +
+                ", holidays=" + holidays +
+                '}';
     }
 }
