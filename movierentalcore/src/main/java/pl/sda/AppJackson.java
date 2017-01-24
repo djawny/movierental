06a77e5955836718f99acdd92129a03a3e75a14e
djawny <djawny@op.pl>
2017-01-24 18:01:01 +0100

    24/01

--- movierentalcore/src/main/java/pl/sda/AppJackson.java
+++ movierentalcore/src/main/java/pl/sda/AppJackson.java
@@ -1,7 +1,30 @@
 package pl.sda;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+
 public class AppJackson {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        // ZAMIANA OBIEKTU NA JSON
+        Customer customer = new Customer("Adam", "Kowalski", 1980, 123);
+        String customerJson = objectMapper.writeValueAsString(customer);
+        System.out.println(customerJson);
+
+        // ZAMIANA JSONa na obiekt
+        String json = "{\n" +
+                "  \"first\\Name\" : \"Adam\",\n" +
+                "  \"lastName\" : \"Kowalski\",\n" +
+                "  \"birthYear\" : 1980,\n" +
+                "  \"idNumber\" : 123\n" +
+                "}";
+        Customer customer1 = objectMapper.readValue(json, Customer.class);
+
 
     }
 }
